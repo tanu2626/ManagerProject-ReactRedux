b0081c8bfdd33ba3ed55bb0dea591d3451ad4ebe
rohitanu <tanu@mysticpants.com>
2017-04-05 15:59:35 +1000

    Added Reducer named AuthReducer

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,9 @@
+// one reducer responsible for all the authentication
+const INITIAL_STATE = { email: '' };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+   default:
+    return state;
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+import AuthReducer from './AuthReducer';
 
 export default combineReducers({
-  //dummy reducer
-  banana: () => []
+  // //dummy reducer
+  // banana: () => []
+  //make a real one noew
+  auth: AuthReducer
 });
