88aa3a83c479fc3d2e31a08e564cd13ba8831bce
rohitanu <tanu@mysticpants.com>
2017-04-13 17:36:19 +1000

    Made employeeReducer

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 
@@ -25,6 +26,17 @@ export const employeeCreate = ({ name, phone, shift }) => {
       dispatch({ type: EMPLOYEE_CREATE });
       Actions.employeeList({ type: 'reset' });
     });
+  };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
   };
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import EmployeeFormReducer from './EmployeeFormReducer';
+import EmployeeReducer from './EmployeeReducer';
 
 export default combineReducers({
   // //dummy reducer
   // banana: () => []
   //make a real one noew
   auth: AuthReducer,
-  employeeForm: EmployeeFormReducer
+  employeeForm: EmployeeFormReducer,
+  employees: EmployeeReducer
 });
